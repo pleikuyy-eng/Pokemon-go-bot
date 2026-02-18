dd156b812d3eb769da6b1bd745f8ab6c605ca51e
pleikuyy-eng <pleikuyy@gmail.com>
2026-02-18 23:00:07 +0700

    Add Express server to respond with bot status

--- index.js
+++ index.js
@@ -29,4 +29,17 @@ client.on("interactionCreate", async interaction => {
   }
 });
 
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
 client.login(process.env.TOKEN);
